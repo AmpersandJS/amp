fe276730a865ac64a8149d436c6d0c9784bc7923
Henrik Joreteg <hjoreteg@gmail.com>
2014-09-23 16:12:42 +0200

    make unique-id attach to the global to handle cases where we can't ensure there isn't another version of this module running

--- unique-id/unique-id.js
+++ unique-id/unique-id.js
@@ -1,9 +1,10 @@
 // Generate a unique integer id (unique within the entire client session).
 // Useful for temporary DOM ids.
-var idCounter = 0;
+var theGlobal = (typeof window !== 'undefined') ? window : global;
+theGlobal.__ampIdCounter = 0;
 
 
 module.exports = function uniqueId(prefix) {
-    var id = ++idCounter + '';
+    var id = ++theGlobal.__ampIdCounter + '';
     return prefix ? prefix + id : id;
 };
