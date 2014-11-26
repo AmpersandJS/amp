2d64d566d17cffc120c897e294313709f2869620
Henrik Joreteg <hjoreteg@gmail.com>
2014-11-26 08:35:07 -0800

    maintain versions custom set in package.json already

--- bin/add-dependencies.js
+++ bin/add-dependencies.js
@@ -10,6 +10,13 @@ modules.forEach(function (mod) {
     mod.dependencies.forEach(function (dep) {
         result[dep] = '^1.0.0';
     });
+
+    // maintain custom-set versions if they already exist
+    for (var item in result) {
+        if (mod.pack.dependencies[item]) {
+            result[item] = mod.pack.dependencies[item];
+        }
+    }
     
     if (size(result)) {
         mod.pack.dependencies = result;
