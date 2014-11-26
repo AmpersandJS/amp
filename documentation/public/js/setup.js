1ae4f6835b8cb6a832f4a111b5bacba21c2773f5
Henrik Joreteg <hjoreteg@gmail.com>
2014-11-25 20:31:31 -0800

    add ability to expand/collapse code sections

--- documentation/public/js/setup.js
+++ documentation/public/js/setup.js
@@ -4,62 +4,76 @@
  */
 
 var prev = new Date().getTime();
-function fallback(fn) {
-  var curr = new Date().getTime();
-  var ms = Math.max(0, 16 - (curr - prev));
-  var req = setTimeout(fn, ms);
-  prev = curr;
-  return req;
-}
+var fallback = function (fn) {
+    var curr = new Date().getTime();
+    var ms = Math.max(0, 16 - (curr - prev));
+    var req = setTimeout(fn, ms);
+    prev = curr;
+    return req;
+};
 
 
 var raf = window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.oRequestAnimationFrame ||
-  window.msRequestAnimationFrame ||
-  fallback;
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    fallback;
 
 // grab all our h* tags
 var aTags = document.querySelectorAll('.nav-docs a');
 var hTags = document.querySelector('.docs-content').querySelectorAll('a.anchor');
 
 function markActive(selected) {
-  var id = selected && selected.name;
-  var found;
-  if (id) {
-    window.selected = selected;
-    Array.prototype.forEach.call(aTags, function (a) {
-      if (a.hash.slice(1) === id) {
-        found = a;
-        a.classList.add('active');
-      } else {
-        a.classList.remove('active');
-      }
-    });
-  }
+    var id = selected && selected.name;
+    var found;
+    if (id) {
+        window.selected = selected;
+        Array.prototype.forEach.call(aTags, function (a) {
+            if (a.hash.slice(1) === id) {
+                found = a;
+                a.classList.add('active');
+            } else {
+                a.classList.remove('active');
+            }
+        });
+    }
 }
 
 function selectCurrent() {
-  var found;
-  var i = 0;
-  var l = hTags.length;
-  var scroll = document.body.scrollTop || window.scrollY;
-  if (scroll < hTags[0].offsetTop) {
-    found = hTags[0];
-  } else {
-    for (; i < l; i++) {
-      if (hTags[i].offsetTop > scroll) {
-        found = hTags[i - 1];
-        break;
-      }
+    var found;
+    var i = 0;
+    var l = hTags.length;
+    var scroll = document.body.scrollTop || window.scrollY;
+    if (scroll < hTags[0].offsetTop) {
+        found = hTags[0];
+    } else {
+        for (; i < l; i++) {
+            if (hTags[i].offsetTop > scroll) {
+                found = hTags[i - 1];
+                break;
+            }
+        }
     }
-  }
-  if (found) markActive(found);
+    if (found) markActive(found);
 }
 
 window.onscroll = function () {
-  raf(selectCurrent);
+    raf(selectCurrent);
 };
+
+document.addEventListener('click', function (e) {
+    var target = e.target;
+    if (target.className === 'toggle') {
+        var next = target.nextElementSibling;
+        if (next.style.display === 'none') {
+            next.style.display = '';
+            target.textContent = target.textContent.replace('Show', 'Collapse');
+        } else {
+            next.style.display = 'none';
+            target.textContent = target.textContent.replace('Collapse', 'Show');
+        }
+    }
+});
 
 selectCurrent();
