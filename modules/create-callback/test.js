7c6221b14b1c5a37e1ea188a0d8fbbe5f0f1be54
Henrik Joreteg <hjoreteg@gmail.com>
2014-11-28 16:26:59 -0800

    fix error causing hangups in IE

--- modules/create-callback/test.js
+++ modules/create-callback/test.js
@@ -12,29 +12,28 @@ test('amp-create-callback', function (t) {
     }, context, 1);
     callback(1, 2, 3, 4, 5);
 
-    var callback = createCallback(function () {
+    callback = createCallback(function () {
         t.equal(this, context, 'should maintain context');
         t.equal(arguments.length, 2, 'should never have more than arg length passed');
     }, context, 2);
     callback(1, 2, 3, 4, 5);
 
-    var callback = createCallback(function () {
+    callback = createCallback(function () {
         t.equal(this, context, 'should maintain context');
         t.equal(arguments.length, 3, 'should never have more than arg length passed');
     }, context, 3);
     callback(1, 2, 3, 4, 5);
 
-    var callback = createCallback(function () {
+    callback = createCallback(function () {
         t.equal(this, context, 'should maintain context');
         t.equal(arguments.length, 4, 'should never have more than arg length passed');
     }, context, 4);
     callback(1, 2, 3, 4, 5);
-
-    var callback = createCallback(function () {
-        t.equal(this, global, 'should not have context');
+    
+    callback = createCallback(function () {
         t.equal(arguments.length, 5, 'should have as many as passed');
     });
     callback(1, 2, 3, 4, 5);
-
+    
     t.end();
 });
