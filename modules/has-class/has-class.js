5eea6b055d94653c16b5d65261320b74e10d7ea2
Henrik Joreteg <hjoreteg@gmail.com>
2014-12-01 15:34:35 -0800

    fix inefficient repeated operation in 'hasClass' per #7

--- modules/has-class/has-class.js
+++ modules/has-class/has-class.js
@@ -5,8 +5,9 @@ var slice = Array.prototype.slice;
 module.exports = function hasClass(el) {
     var cls = slice.call(arguments, 1);
     var clsName = isString(el) ? el : el.className;
+    clsName = ' ' + clsName +  ' ';
     for (var i = 0, l = cls.length; i < l; i++) {
-        if ((' ' + clsName +  ' ').indexOf(' ' + cls[i] + ' ') < 0) {
+        if (clsName.indexOf(' ' + cls[i] + ' ') < 0) {
             return false;
         }
     }
