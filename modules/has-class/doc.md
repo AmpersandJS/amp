b7de451acd2ad14d4469904e77be2cf13ecf90ba
Henrik Joreteg <hjoreteg@gmail.com>
2014-12-02 08:42:15 -0800

    hasClass no longer takes more than one class to look for. ref #7

--- modules/has-class/doc.md
+++ modules/has-class/doc.md
@@ -1,3 +1,3 @@
-Returns `true` if the `element` has the class(es) and `false` otherwise. You can pass as many classes as you want. 
+Returns `true` if the `element` has the class and `false` otherwise.
 
 If the first argument is a string, it is assumed to be what you'd get from `el.className`. This allows it to be used repeatedly for a single element without reading from the DOM more than once.
--- modules/has-class/has-class.js
+++ modules/has-class/has-class.js
@@ -1,15 +1,7 @@
 var isString = require('../is-string');
-var slice = Array.prototype.slice;
 
 
-module.exports = function hasClass(el) {
-    var cls = slice.call(arguments, 1);
+module.exports = function hasClass(el, cls) {
     var clsName = isString(el) ? el : el.className;
-    clsName = ' ' + clsName +  ' ';
-    for (var i = 0, l = cls.length; i < l; i++) {
-        if (clsName.indexOf(' ' + cls[i] + ' ') < 0) {
-            return false;
-        }
-    }
-    return true;
+    return (' ' + clsName + ' ').indexOf(' ' + cls + ' ') !== -1;
 };
