120502dceffc457d8b8897872e2a55dd6882a862
Michael Garvin <gar@andyet.net>
2014-12-11 14:32:47 -0800

    use negate module

--- modules/omit/omit.js
+++ modules/omit/omit.js
@@ -3,13 +3,8 @@ var map = require('amp-map');
 var contains = require('amp-contains');
 var flatten = require('amp-flatten');
 var pick = require('amp-pick');
+var negate = require('amp-negate');
 
-//Should this be amp-negate?
-function negate(context) {
-    return function() {
-        return !context.apply(this, arguments);
-    };
-}
 module.exports = function omit(obj, iteratee, context) {
     if (isFunction(iteratee)) {
         iteratee = negate(iteratee);
