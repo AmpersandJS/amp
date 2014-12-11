c3baf2f1e08f4d005c299f52291f16b66db17508
Michael Garvin <gar@andyet.net>
2014-12-10 16:30:41 -0800

    add test for value not found

--- modules/find/test.js
+++ modules/find/test.js
@@ -12,17 +12,14 @@ test('amp-find', function (t) {
     t.equal(find(evenArray, isEven), 2, 'finds first even value');
     t.equal(find(evenObject, isEven), 2, 'can filter objects');
 
+    t.equal(find(evenObject, function () { return false; }), undefined, 'returns undefined if value not found');
+
     find([1], function () {
         t.equal(this, evenObject, 'given context');
     }, evenObject);
 
     // Can be used like amp-find-where.
-    var list = [
-        {a: 1, b: 2},
-        {a: 2, b: 2},
-        {a: 1, b: 3},
-        {a: 1, b: 4}
-    ];
+    var list = [ {a: 1, b: 2}, {a: 2, b: 2}, {a: 1, b: 3}, {a: 1, b: 4} ];
 
     t.deepEqual(find(list, {a: 1}), {a: 1, b: 2});
 
