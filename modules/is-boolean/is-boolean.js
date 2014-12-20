a2d330202f904532df6fc55e53d9de7ef7779da7
Henrik Joreteg <hjoreteg@gmail.com>
2014-12-19 23:44:56 -0800

    Also support new Boolean(false) form for isBoolean. closes #31 (thanks @jdalton)

--- modules/is-boolean/is-boolean.js
+++ modules/is-boolean/is-boolean.js
@@ -1,3 +1,6 @@
+var toString = Object.prototype.toString;
+
+
 module.exports = function isBoolean(obj) {
-    return obj === true || obj === false;
+    return obj === true || obj === false || toString.call(obj) === '[object Boolean]';
 };
