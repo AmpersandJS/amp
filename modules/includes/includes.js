8bc50664d2c6497a59630d76fd9827e7a3ef90c9
Henrik Joreteg <hjoreteg@gmail.com>
2015-01-05 11:55:22 -0800

    optimize 'includes' to use from argument in indexOf. Closes #29

--- modules/includes/includes.js
+++ modules/includes/includes.js
@@ -2,8 +2,5 @@ var indexOf = String.prototype.indexOf;
 
 
 module.exports = function includes(string, query, position) {
-    if (position) {
-        string = string.slice(position);
-    }
-    return indexOf.call(string, query) !== -1;
+    return indexOf.call(string, query, position) !== -1;
 };
