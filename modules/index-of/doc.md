8e230f1e0d45b23dd2c99ee3a172cbf619539a9f
Henrik Joreteg <hjoreteg@gmail.com>
2015-01-05 14:21:48 -0800

    add support for fromIndex to index-of. Also no longer forks. Closes #30. Closes #44

--- modules/index-of/doc.md
+++ modules/index-of/doc.md
@@ -1,3 +1,1 @@
-Returns the index at which the item can be found in the array (also works for `arguments` object). Returns `-1` if value is not present. Uses native `Array.prototype.indexOf` if present. But, unlike native implementations this won't throw an error if passed something other than an array, it'll just return `-1`. 
-
-*note:* This is not intended for use with sparse arrays as there will be some inconsistencies in how those are handled in the fallback implementation for old IEs.
+Returns the index at which the item can be found in the array (also works for `arguments` object). Returns `-1` if value is not present. Unlike native implementations this won't throw an error if passed something other than an array, it'll just return `-1`. You can optionally pass a `fromIndex` as the third argument to start the search there.
--- modules/index-of/test.js
+++ modules/index-of/test.js
@@ -1,5 +1,7 @@
 var test = require('tape');
 var indexOf = require('./index-of');
+var isArray = require('amp-is-array');
+var each = require('amp-each');
 
 
 test('amp-index-of', function (t) {
@@ -13,5 +15,31 @@ test('amp-index-of', function (t) {
     t.equal(indexOf({}, 1), -1);
     t.equal(indexOf(['a'], 'a'), 0);
     t.equal(indexOf(['a' , 1], 1), 1);
+    
+    each([null, void 0, [], false], function(val) {
+        var msg = 'Handles: ' + (isArray(val) ? '[]' : val);
+        t.equal(indexOf(val, 2), -1, msg);
+        t.equal(indexOf(val, 2, -1), -1, msg);
+        t.equal(indexOf(val, 2, -20), -1, msg);
+        t.equal(indexOf(val, 2, 15), -1, msg);
+    });
+
+    var numbers = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    t.equal(indexOf(numbers, 2, 5), 7, 'supports the fromIndex argument');
+
+    index = indexOf([,,,], undefined);
+    t.equal(index, 0, 'treats sparse arrays as if they were dense');
+
+    var array = [1, 2, 3, 1, 2, 3];
+    t.strictEqual(indexOf(array, 1, -3), 3, 'neg `fromIndex` starts at the right index');
+    t.strictEqual(indexOf(array, 1, -2), -1, 'neg `fromIndex` starts at the right index');
+    t.strictEqual(indexOf(array, 2, -3), 4);
+    each([-6, -8, -Infinity], function(fromIndex) {
+      t.strictEqual(indexOf(array, 1, fromIndex), 0);
+    });
+    t.strictEqual(indexOf([1, 2, 3], 1, true), 0);
+
     t.end();
 });
+
+
