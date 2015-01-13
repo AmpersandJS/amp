d1307ff6421239644bd737610d2b67a6af6858c8
jvduf <cloud.sync.account@gmail.com>
2015-01-13 23:24:16 +0100

    Tweaked `next` example

--- modules/next/example.js
+++ modules/next/example.js
@@ -2,8 +2,8 @@ var next = require('amp-next');
 
 var arr = ['a', 'b', 'c'];
 
-console.log(next(arr, 'a')); //=> b
-console.log(next(arr, 'b')); //=> c
+next(arr, 'a'); //=> b
+next(arr, 'b'); //=> c
 
 // when the last item of the array is specified to fetch the next item from the first item of the array will be returned
-console.log(next(arr, 'c')); //=> a
+next(arr, 'c'); //=> a
