110d15e6cf5564707c395a7199b8d74e077282f8
Richard Torruellas <rtorruellas@gmail.com>
2015-01-16 14:21:08 -0700

    remove sort

--- modules/merge/merge.js
+++ modules/merge/merge.js
@@ -1,7 +1,7 @@
 var extend = require('amp-extend');
 
 module.exports = function merge() {
-    var args = [].slice.call(arguments).sort();
+    var args = [].slice.call(arguments);
     args.unshift({});
     return extend.apply(this, args);
 };
