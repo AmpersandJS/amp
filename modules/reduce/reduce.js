865ca4e6b8aba88f1206a2dcfe9c5faea2fb5c6b
Henrik Joreteg <hjoreteg@gmail.com>
2015-01-16 16:15:37 -0800

    a few code style tweaks in 'reduce'

--- modules/reduce/reduce.js
+++ modules/reduce/reduce.js
@@ -5,16 +5,19 @@ var objKeys = require('amp-keys');
 module.exports = function reduce(obj, iteratee, memo, context) {
     if (obj == null) obj = [];
     iteratee = createCallback(iteratee, context, 4);
-    var keys = obj.length !== +obj.length && objKeys(obj),
-        length = (keys || obj).length,
-        index = 0, currentKey;
+    var keys = obj.length !== +obj.length && objKeys(obj);
+    var length = (keys || obj).length;
+    var index = 0;
+    var currentKey;
     if (arguments.length < 3) {
-      if (!length) throw new TypeError('Reduce of empty array with no initial value');
-      memo = obj[keys ? keys[index++] : index++];
+        if (!length) {
+            throw new TypeError('Reduce of empty array with no initial value');
+        }
+        memo = obj[keys ? keys[index++] : index++];
     }
     for (; index < length; index++) {
-      currentKey = keys ? keys[index] : index;
-      memo = iteratee(memo, obj[currentKey], currentKey, obj);
+        currentKey = keys ? keys[index] : index;
+        memo = iteratee(memo, obj[currentKey], currentKey, obj);
     }
     return memo;
 };
