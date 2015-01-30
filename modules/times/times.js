e2b38cc068d119b9d0d903bd68c14be1b0fda14b
Michael Garvin <gar@andyet.net>
2015-01-30 08:31:36 -0800

    move to functions, minor formatting

--- modules/times/times.js
+++ modules/times/times.js
@@ -1,5 +1,6 @@
 var createCallback = require('amp-create-callback');
 
+
 module.exports = function times(n, iteratee, context) {
     var accum = Array(Math.max(0, n));
     iteratee = createCallback(iteratee, context, 1);
