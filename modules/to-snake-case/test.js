6748d09bda61db89353bf11470644f4ff2ade25e
David La Chasse <david.lachasse@gmail.com>
2015-03-15 17:19:10 -0700

    Missing semicolons
    Garbage mistake

--- modules/to-snake-case/test.js
+++ modules/to-snake-case/test.js
@@ -1,7 +1,6 @@
 var test = require('tape');
 var toSnakeCase = require('./to-snake-case');
 
-
 test('amp-to-snake-case', function (t) {
 	t.equal(toSnakeCase('!!!'), '');
 	t.equal(toSnakeCase([]), '');
@@ -10,8 +9,8 @@ test('amp-to-snake-case', function (t) {
 	t.equal(toSnakeCase('on a plane'), 'on_a_plane');
 	t.equal(toSnakeCase('on a mother friendly? plane', true), 'ON_A_MOTHER_FRIENDLY_PLANE');
 	t.equal(toSnakeCase('_  they\'re   EVERYWHERE!!'), 'they_re_everywhere');
-	t.equal(toSnakeCase('tired o\' these snakes'), 'tired_o_these_snakes')
+	t.equal(toSnakeCase('tired o\' these snakes'), 'tired_o_these_snakes');
 	t.equal(toSnakeCase('test------ssssnakes___!'), 'test_ssssnakes');
-	t.equal(toSnakeCase('no mo snakes', true), 'NO_MO_SNAKES')
+	t.equal(toSnakeCase('no mo snakes', true), 'NO_MO_SNAKES');
   t.end();
 });
